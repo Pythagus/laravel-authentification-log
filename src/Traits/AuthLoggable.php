8e13de2fff48cc1f75ae42f088f1f87bbcbccb99
Damien MOLINA <dmolina@etud.insa-toulouse.fr>
2020-09-09 08:15:06 +0200

    Adding hasLastLogin method

--- src/Traits/AuthLoggable.php
+++ src/Traits/AuthLoggable.php
@@ -16,36 +16,55 @@ use Pythagus\LaravelAuthentificationLog\Models\UserLogin;
  */
 trait AuthLoggable {
 
-    /**
-     * Get all the user's logins.
-     *
-     * @return BelongsTo
-     */
-    public function logins() {
-        return $this->hasMany(UserLogin::class, 'user_id')->orderByDesc('created_at') ;
-    }
+	/**
+	 * Get all the user's logins.
+	 *
+	 * @return BelongsTo
+	 */
+	public function logins() {
+		return $this->hasMany(UserLogin::class, 'user_id')->orderByDesc('created_at') ;
+	}
 
-    /**
-     * Get the last instance of user's login.
-     * We skip the first data because the last
-     * user's connection is before the last one.
-     *
-     * @return UserLogin|null
-     */
-    public function lastLogin() {
-        return $this->lastLoginAtPosition(1) ;
-    }
+	/**
+	 * Get the last instance of user's login.
+	 * We skip the first data because the last
+	 * user's connection is before the last one.
+	 *
+	 * @return UserLogin|null
+	 */
+	public function lastLogin() {
+		return $this->lastLoginAtPosition(1) ;
+	}
 
-    /**
-     * Get the last instance of user's login.
-     * We skip the first data because the last
-     * user's login is before the last one.
-     *
-     * @param int $position
-     * @return UserLogin
-     */
-    public function lastLoginAtPosition(int $position = 0) {
-        return $this->logins->skip($position)->first() ;
-    }
+	/**
+	 * Get the last instance of user's login.
+	 * We skip the first data because the last
+	 * user's login is before the last one.
+	 *
+	 * @param int $position
+	 * @return UserLogin
+	 */
+	public function lastLoginAtPosition(int $position = 1) {
+		return $this->lastLoginScope($position)->first() ;
+	}
+
+	/**
+	 * Determine whether the user already has
+	 * a UserLogin instance at the given position.
+	 *
+	 * @param int $position
+	 * @return bool
+	 */
+	public function hasLastLogin(int $position = 1) {
+		return $this->lastLoginScope($position)->isNotEmpty() ;
+	}
+
+	/**
+	 * @param int $position
+	 * @return Collection
+	 */
+	public function lastLoginScope(int $position) {
+		return $this->logins->skip($position) ;
+	}
 
 }
